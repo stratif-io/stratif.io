efcd10bcaec789f9e0f999e89fc8a43a03dc5674
Carlo Abi Chahine <carlo.abichahine@gmail.com>
2026-04-17 11:57:27 +0200

    chore(docs): scaffold Starlight app

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+export default defineConfig({
+  site: 'https://docs.stratif.io',
+  integrations: [
+    starlight({
+      title: 'stratif.io',
+      logo: {
+        light: './src/assets/logo-light.svg',
+        dark: './src/assets/logo-dark.svg',
+        replacesTitle: true,
+      },
+      favicon: '/favicon.svg',
+      customCss: ['./src/styles/custom.css'],
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/stratif-io/stratif.io' },
+      ],
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            { label: 'Introduction', slug: '' },
+            { label: 'Installation', slug: 'getting-started/installation' },
+            { label: 'Configuration', slug: 'getting-started/configuration' },
+            { label: 'Connecting a Warehouse', slug: 'getting-started/connecting-a-warehouse' },
+          ],
+        },
+        {
+          label: 'Demo',
+          items: [
+            { label: 'Sample Data', slug: 'demo/sample-data' },
+          ],
+        },
+      ],
+    }),
+  ],
+})
