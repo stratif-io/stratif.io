c73341d96ff435e357c6afb1503f9e0cc9010c57
Carlo Abi Chahine <carlo.abichahine@gmail.com>
2026-04-21 10:56:28 +0200

    fix(seeder-studio): add min-w-0 to main column to prevent flex overflow

--- apps/seeder-studio/src/App.tsx
+++ apps/seeder-studio/src/App.tsx
@@ -119,7 +119,7 @@ export default function App() {
       <div className="flex flex-1 min-h-0 overflow-hidden">
         <AxisSidebar />
 
-        <main className="flex-1 flex flex-col min-h-0 overflow-hidden">
+        <main className="flex-1 min-w-0 flex flex-col min-h-0 overflow-hidden">
           <AnomaliesPane />
           <div className="flex-1 min-h-0">
             <PreviewGrid />
