23d9c70471b398dceeec498255da620b250bec6f
Carlo Abi Chahine <carlo.abichahine@gmail.com>
2026-04-22 08:14:06 +0200

    fix(layout): make preview grid scroll by adding flex flex-col to wrapper div

--- apps/seeder-studio/src/App.tsx
+++ apps/seeder-studio/src/App.tsx
@@ -140,7 +140,7 @@ export default function App() {
         </ErrorBoundary>
 
         <main className="flex-1 min-w-0 flex flex-col min-h-0 overflow-hidden">
-          <div className="flex-1 min-h-0">
+          <div className="flex-1 min-h-0 flex flex-col">
             <ErrorBoundary>
               <PreviewGrid />
             </ErrorBoundary>
